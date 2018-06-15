0cc8669d1a3d63c3defadcf07b2febfb161b3984
sarathi0333 <sarath.kct@gmail.com>
2018-06-15 08:53:43 +0530

    Welcome component UI work done

--- frontend/src/app/components/welcome/welcome.component.ts
+++ frontend/src/app/components/welcome/welcome.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrollTopService } from './../../services/shared/scrollToTop/scroll-top.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
+  providers: [ScrollTopService]
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  languages:string[] = ["English", "தமிழ்", "മലയാളം", "తెలుగు"];
+  langOption: string;
+  constructor(private scrollToTop: ScrollTopService) { }
 
   ngOnInit() {
+    this.langOption = "English";
+    this.scrollToTop.setScrollTop();
   }
 
 }
