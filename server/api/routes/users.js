c6da87783e65e77ce2c8ea0d7c285ed3e41366c5
sarathi0333 <sarath.kct@gmail.com>
2018-06-17 22:51:54 +0530

    xlsx decode to json added in user.js

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+var XLSX = require('xlsx');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -12,12 +13,22 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({
-    storage: storage
+    storage: storage,
+    fileFilter: function(req, file, cb) {
+        if(['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
+            return callback(new Error('Wrong extension type'));
+        }
+        cb(null, true);
+    }
   });
 
 // accept user data excel file in this route
 router.post('/user', upload.single('userData'), (req, res, next) => {
     console.log(req.file);
+    var workbook = XLSX.readFile('./uploads/' + req.file.filename);
+    var sheet_name_list = workbook.SheetNames;
+    var data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+    data = JSON.parse(data);
     res.status(200).json({
         message: 'File has been uploaded successfully'
     });
