506150bebbbe2295588bba1023b60d3e122d4a16
sarathi0333 <sarath.kct@gmail.com>
2018-06-17 16:35:51 +0530

    added user_id ref in zone details

--- server/api/models/zone.js
+++ server/api/models/zone.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const zoneDetailsSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'UserDetails', required: true },
     zone_name: { type: String, required: true },
     site_id: String,
     site_name: String,
